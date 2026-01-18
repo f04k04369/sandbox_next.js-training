71491d4819383748234de03af0bc2ac86f19fccc
f04k04369 <curb369@gmail.com>
2026-01-18 22:51:23 +0900

    メニュー画面open、close実装・shadcnui導入

---
+++ src/components/menu-sheet.tsx
@@ -0,0 +1,33 @@
+
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export default function MenuSheet() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant={"ghost"} size={"icon"}>
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="w-72">
+        <SheetHeader className="sr-only">
+          <SheetTitle>メニュー情報</SheetTitle>
+          <SheetDescription>
+            ユーザー情報とメニュー情報を表示  
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  )
+}
