b383d551f5b507221ced1a812cd997b76deeef5a
kosukefujisawa <curb369@gmail.com>
2026-04-29 22:01:02 +0900

    カテゴリのプロパティ受け渡し確認

--- src/components/text-toggle-button.tsx
+++ src/components/text-toggle-button.tsx
@@ -5,7 +5,8 @@ import { Button } from './ui/button'
 export default function TextToggleButton() {
     const[isExpanded, setIsExpanded] = useState(false);
     const handleChange = () => {
-        setIsExpanded((prev) => !prev);    }
+      setIsExpanded((prev) => !prev);    
+    };
   return (
     <Button onClick={handleChange}>{isExpanded ? "表示を戻す" : "全て表示"}</Button>
   )
