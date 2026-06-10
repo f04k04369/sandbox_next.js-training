dd8159304732c5ded1b520d103d3a6d21e398004
kosukefujisawa <curb369@gmail.com>
2026-06-10 23:43:30 +0900

    96_addressesテーブルに住所を保存しよう

--- src/app/(private)/actions/addressActions.ts
+++ src/app/(private)/actions/addressActions.ts
@@ -1,13 +1,15 @@
 "use server";
 import { AddressSuggestion } from "@/types";
 import { getPlaceDetails } from "@/lib/restaurants/api";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
 export async function selectSuggestionAction(
   suggestion: AddressSuggestion,
   sessionToken: string,
 ) {
-
-    throw new Error("getPlaceDetailsえらー");
+  const supabase = await createClient();
+  throw new Error("getPlaceDetailsえらー");
   console.log("selectSuggestionAction", suggestion);
 
   const { data: locationData, error } = await getPlaceDetails(
@@ -20,9 +22,31 @@ export async function selectSuggestionAction(
   if (
     error ||
     !locationData?.location ||
-    !locationData.location.latitude ||
-    !locationData.location.longitude
+    !locationData?.location?.latitude ||
+    !locationData?.location?.longitude
   ) {
     throw new Error("住所の取得に失敗しました");
   }
+  const {
+    data: { user },
+    error: userError,
+  } = await supabase.auth.getUser();
+  
+  if(userError || !user) {
+    redirect("/login");
+  }
+    // データベースに保存処理
+
+    const { error: insertError } = await supabase.from("addresses").insert({
+        name: suggestion.placeName,
+        address_text: suggestion.address_text,
+        latitude: locationData?.location?.latitude,
+        longitude: locationData?.location?.longitude,
+        user_id: user.id,
+    });
+
+    if(insertError) {
+        console.error("住所の保存に失敗しました",insertError);
+        throw new Error("住所の保存に失敗しました");
+    }
 }
