20f28f70d7fd120965e38fda57b74fd8bb1fc0a9
kosukefujisawa <curb369@gmail.com>
2026-07-14 23:51:59 +0900

    135_画面スクロールでサイドバーのカテゴリに背景色をつけよう

--- src/components/category-sidebar.tsx
+++ src/components/category-sidebar.tsx
@@ -1,21 +1,35 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { CategoryMenu } from "@/types";
 import React from "react";
 
 interface CategorySidebarProps {
   categoryMenus: CategoryMenu[];
+  onSelectCategory: (categoryId: string) => void;
+  activeCategoryId: string;
 }
 
 export default function CategorySidebar({
   categoryMenus,
+  onSelectCategory,
+  activeCategoryId,
 }: CategorySidebarProps) {
   return (
-    <aside className="w-1/4 bg-blue-500">
+    <aside className="w-1/4 sticky top-16 h-[calc(100vh-64px)]">
       <p className="p-3 font-bold">メニュー Menu</p>
       <nav>
         <ul>
           {categoryMenus.map((category) => (
             <li key={category.id}>
-              <button type="button" className="bg-red-100 w-full p-4 text-left">{category.categoryName}</button>
+              <button
+                onClick={() => onSelectCategory(category.id)}
+                type="button"
+                className = {cn("w-full p-4 text-left border-l-4",
+                  activeCategoryId === category.id ? "bg-input font-medium border-l-4 border-primary" : "border-transparent transition-colors hover:bg-muted"
+                )}
+              >
+                {category.categoryName}
+              </button>
             </li>
           ))}
         </ul>
--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -1,31 +1,65 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import CategorySidebar from "./category-sidebar";
 import { CategoryMenu } from "@/types";
 import Section from "./section";
 import CarouselContainer from "./carousel-container";
 import MenuCard from "./menu-card";
+import FlatMenuCard from "./flat-menu-card";
+import { InView } from "react-intersection-observer";
 
 interface MenuContentProps {
   categoryMenus: CategoryMenu[];
 }
 
 export default function MenuContent({ categoryMenus }: MenuContentProps) {
+  const [activeCategoryId, setActiveCategoryId] = useState(categoryMenus[0].id);
+  const handleSelectCategory = (categoryId: string) => {
+    setActiveCategoryId(categoryId);
+    console.log("categoryId: ", categoryId);
+
+    const element = document.getElementById(`${categoryId}-menu`);
+    console.log("element: ", element);
+
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="flex gap-4">
-      <CategorySidebar categoryMenus={categoryMenus} />
+      <CategorySidebar
+        categoryMenus={categoryMenus}
+        onSelectCategory={handleSelectCategory}
+        activeCategoryId={activeCategoryId}
+      />
       <div className="w-3/4 bg-red-500">
         {categoryMenus.map((category) => (
-          <Section key={category.id} title={category.categoryName}>
-            {category.id === "featured" ? (
-              <CarouselContainer slideToShow={4}>
-                {category.items.map((menu) => (
-                  <MenuCard menu={menu} />
-                ))}
-              </CarouselContainer>
-            ) : (
-              <div>リストメニュー</div>
-            )}
-          </Section>
+            <InView
+              as="div"
+              onChange={(inView, entry) => inView && setActiveCategoryId(category.id)}
+              key={category.id}
+              id={`${category.id}-menu`}
+              className="scroll-mt-16"
+              threshold={0.7}
+  
+            >
+              <Section title={category.categoryName}>
+                {category.id === "featured" ? (
+                  <CarouselContainer slideToShow={4}>
+                    {category.items.map((menu) => (
+                      <MenuCard menu={menu} />
+                    ))}
+                  </CarouselContainer>
+                ) : (
+                  <div className="grid grid-cols-2 gap-4">
+                    {category.items.map((menu) => (
+                      <FlatMenuCard key={menu.id} menu={menu} />
+                    ))}
+                  </div>
+                )}
+              </Section>
+            </InView>
         ))}
       </div>
     </div>
